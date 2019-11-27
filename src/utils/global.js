bb1fe82cb02bca8f9753ff4737dc98dd85164c48
马腾 <1163182715@qq.com>
2019-11-27 19:00:30 +0800

    临时更新

--- src/utils/global.js
+++ src/utils/global.js
@@ -2,7 +2,7 @@ export default {
   GITHUB_API_HOST: 'https://api.github.com',
   USER: 'MissThee',
   REPOSITORY: 'page',
-  TOKEN: '',
+  TOKEN: '9119e67affbaee24a0505e447abb163c2843434c', //暂用一个没有任何权限的token
   DOC_ROOT: '/doc',
   HOME_PAGE: '/home/home.md',
 };
