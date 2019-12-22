7cab1caf078337dfbe8c79bb952fffff68f9cbd6
Ma Teng <1163182715@qq.com>
2019-12-22 11:45:21 +0800

    静态页测试

--- src/home/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App';
-Vue.config.devtools = true;//使浏览器调试插件可用
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>',
-});
--- vue.config.js
+++ vue.config.js
@@ -5,21 +5,14 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
-
   pages: {//多入口
-    "dir1/home": {//如果写目录，打包后会增加目录
-      entry: 'src/home/main.js',//入口js
-      template: 'public/index.html',//入口模板。（webpack会将public下的文件拷贝到输出目录）
-      filename: 'index.html',//打包后的名字，可写成路径。
-    },
     docviewer: {
       entry: 'src/docviewer/main.js',
       template: 'public/docviewer/index.html',
       filename: 'docviewer/index.html',
     },
-
   },
-  outputDir: 'main', // 打包文件输出路径，即打包到哪里
+  outputDir: 'dist', // 打包文件输出路径，即打包到哪里
   assetsDir: 'static',// 静态资源地址
   lintOnSave: false, // eslint-loader 是否在保存的时候检查
   productionSourceMap: true,  // 生产环境是否生成 sourceMap 文件
