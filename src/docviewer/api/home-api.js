2937ddda8884a697b527e3bb8da273c088aa89e8
马腾 <1163182715@qq.com>
2020-04-20 15:47:35 +0800

    更新

--- src/docviewer/api/home-api.js
+++
@@ -1,9 +0,0 @@
-import request from 'src/docviewer/utils/request';
-import Global from 'src/docviewer/utils/global';
-
-export default {
-  getFileFromGithub: requestData => request({
-    method: 'get',
-    url:  requestData.url,
-  }),
-};
